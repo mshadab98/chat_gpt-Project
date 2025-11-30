586eb081e5a491b80570dafd08106305faca0ca0
mshadab98 <mohdshadab98977@gmail.com>
2025-11-30 12:52:22 +0530

    added middlewares in between user and socket.io connection

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -1,7 +1,33 @@
 const {Server} = require('socket.io');
+const cookie = require('cookie');
+const jwt  = require('jsonwebtoken')
+const userModel = require('../models/user.models')
+
 
 function initSocketServer(httpServer) {
     const io = new Server(httpServer, {})
+
+    io.use(async(socket, next) => {
+
+        const cookie = cookie.parse(socket.handshake.headers?.cookie || "");
+        console.log("socket connection cookie", cookie)
+
+
+        if(!cookie.token) {
+            next(new Error("Authentication error: No token provided"))
+        }
+
+        try {
+            const decoded = jwt.verify(cookie.token, process.env.JWT_SECRET)
+           const user = await userModel.findById(decoded.id)
+            socket.user = user
+            next()
+        }
+        catch(err){
+            next(new Error("Authentication error: Invalid token"))
+
+        }
+    })
 
     io.on("connection", (socket) => {
         console.log("New socket connection:", socket.id)
